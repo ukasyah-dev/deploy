3ed35d9fb80fde19d3fc517b4504bcc810be4f8e
Ukasyah <hi.ukasyah@gmail.com>
2024-07-25 11:10:39 +0700

    wip

--- main.go
+++ main.go
@@ -3,14 +3,38 @@ package main
 import (
 	"fmt"
 	"log"
+	"time"
 
 	"github.com/caitlinelfring/go-env-default"
+	"github.com/ukasyah-dev/"
 	"github.com/gofiber/fiber/v2"
+	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/clientcmd"
 )
 
 var httpPort = env.GetIntDefault("HTTP_PORT", 3000)
+var inCluster = env.GetBoolDefault("IN_CLUSTER", false)
+var kubeconfig = env.GetDefault("KUBECONFIG", "~/.kube/config")
 
 func main() {
+	var config *rest.Config
+	var err error
+
+	if !inCluster {
+		config, err = clientcmd.BuildConfigFromFlags("", kubeconfig)
+		if err != nil {
+			panic(err)
+		}
+	}
+
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		panic(err)
+	}
+
 	app := fiber.New()
 
 	app.Get("/", func(c *fiber.Ctx) error {
@@ -18,6 +42,14 @@ func main() {
 	})
 
 	app.Post("/:name", func(c *fiber.Ctx) error {
+		deploymentName := c.Params("name")
+
+		deploymentsClient := clientset.AppsV1().Deployments("default")
+		data := fmt.Sprintf(`{"spec": {"template": {"metadata": {"annotations": {"kubectl.kubernetes.io/restartedAt": "%s"}}}}}`, time.Now().Format("20060102150405"))
+		deployment, err := deploymentsClient.Patch(c.Context(), deploymentName, types.StrategicMergePatchType, []byte(data), v1.PatchOptions{})
+		if err != nil {
+			log.Errorf("Failed to patch deployment: %s", err)
+
 		return c.JSON(fiber.Map{"message": "TODO: Deploy " + c.Params("name")})
 	})
 
