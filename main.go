e11de8c82c38e46f2bd27441080c0154a55ecace
Ukasyah <hi.ukasyah@gmail.com>
2024-07-25 13:05:54 +0700

    add env

--- main.go
+++ main.go
@@ -1,13 +1,14 @@
 package main
 
 import (
+	"context"
 	"fmt"
-	"log"
 	"time"
 
+	"github.com/appleboy/graceful"
 	"github.com/caitlinelfring/go-env-default"
-	"github.com/ukasyah-dev/"
 	"github.com/gofiber/fiber/v2"
+	"github.com/ukasyah-dev/common/log"
 	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/types"
 	"k8s.io/client-go/kubernetes"
@@ -23,7 +24,12 @@ func main() {
 	var config *rest.Config
 	var err error
 
-	if !inCluster {
+	if inCluster {
+		config, err = rest.InClusterConfig()
+		if err != nil {
+			panic(err)
+		}
+	} else {
 		config, err = clientcmd.BuildConfigFromFlags("", kubeconfig)
 		if err != nil {
 			panic(err)
@@ -44,14 +50,27 @@ func main() {
 	app.Post("/:name", func(c *fiber.Ctx) error {
 		deploymentName := c.Params("name")
 
-		deploymentsClient := clientset.AppsV1().Deployments("default")
-		data := fmt.Sprintf(`{"spec": {"template": {"metadata": {"annotations": {"kubectl.kubernetes.io/restartedAt": "%s"}}}}}`, time.Now().Format("20060102150405"))
-		deployment, err := deploymentsClient.Patch(c.Context(), deploymentName, types.StrategicMergePatchType, []byte(data), v1.PatchOptions{})
+		client := clientset.AppsV1().Deployments("default")
+		data := fmt.Sprintf(
+			`{"spec": {"template": {"metadata": {"annotations": {"kubectl.kubernetes.io/restartedAt": "%s"}}}}}`,
+			time.Now().Format("20060102150405"),
+		)
+		deployment, err := client.Patch(c.Context(), deploymentName, types.StrategicMergePatchType, []byte(data), v1.PatchOptions{})
 		if err != nil {
 			log.Errorf("Failed to patch deployment: %s", err)
+			return c.JSON(fiber.Map{"message": "Failed to patch deployment"})
+		}
 
-		return c.JSON(fiber.Map{"message": "TODO: Deploy " + c.Params("name")})
+		return c.JSON(fiber.Map{"message": "Deployment patched", "deployment": deployment})
 	})
 
-	log.Fatal(app.Listen(fmt.Sprintf(":%d", httpPort)))
+	m := graceful.NewManager()
+
+	m.AddRunningJob(func(ctx context.Context) error {
+		return app.Listen(fmt.Sprintf(":%d", httpPort))
+	})
+
+	m.AddShutdownJob(func() error {
+		return app.Shutdown()
+	})
 }
